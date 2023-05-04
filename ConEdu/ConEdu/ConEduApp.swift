373a1692dc3309a507b4321c16fb7aa3c839b202
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-05-04 21:54:37 +0700

    Initial Files

---
+++ ConEdu/ConEdu/ConEduApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConEduApp.swift
+//  ConEdu
+//
+//  Created by MacBook Pro on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ConEduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
