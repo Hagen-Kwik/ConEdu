f564471af485366d9b33d5264e7e622ef530d4c5
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-05-07 14:12:18 +0700

    tabview

---
+++ ConEdu/ConEdu/View/Home_View.swift
@@ -0,0 +1,20 @@
+//
+//  Home_View.swift
+//  ConEdu
+//
+//  Created by MacBook Pro on 07/05/23.
+//
+
+import SwiftUI
+
+struct Home_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Home_View()
+    }
+}
---
+++ ConEdu/ConEdu/View/Profile_View.swift
@@ -0,0 +1,20 @@
+//
+//  Profile_View.swift
+//  ConEdu
+//
+//  Created by MacBook Pro on 07/05/23.
+//
+
+import SwiftUI
+
+struct Profile_View: View {
+    var body: some View {
+        Text("profile")
+    }
+}
+
+struct Profile_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Profile_View()
+    }
+}
